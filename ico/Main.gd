1608648746f61cd41869d22a5cf46d27e1178dd8
dzil123 <5725958+dzil123@users.noreply.github.com>
2022-02-07 01:42:09 -0800

    ico movement test

--- ico/Main.gd
+++ ico/Main.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
 #onready var grid = $grid2
-#onready var ico = $AnimationPlayer/ico
+onready var ico = $rotatesource
 
 # distance between two equilateral triangle centers
 # https://en.wikipedia.org/wiki/Equilateral_triangle#Principal_properties
@@ -15,6 +15,8 @@ const RADIUS_VERTEX: float = sqrt(10.0 + 2.0 * sqrt(5.0)) / 4.0
 const RADIUS_FACE: float = (sqrt(3.0) * (3.0 + sqrt(5.0))) / 12.0
 # midradius
 const RADIUS_EDGE: float = (1.0 + sqrt(5.0)) / 4.0
+# 180 - dihedral angle
+const ANGLE = PI - acos(sqrt(5.0) / -3.0)
 
 var tris = [
 	Vector3(1, 0, 0),
@@ -25,12 +27,25 @@ var tris = [
 	Vector3(1, 0, 1)
 ]
 
-var tri_pos = Vector3(1, 4, -3)
+const TRI_POS_START = Vector3(1, 4, -3)
+var tri_pos = TRI_POS_START
 
 var index = 0
 
+# eg a movement of (1,0,0) is rotation by ANGLE on axis rotation_mat3.x
+var rotation_mat3 = Basis(
+	Vector3(cos(PI / 3.0), 0, -sin(PI / 3.0)),
+	Vector3(-1, 0, 0),
+	Vector3(cos(PI / 3.0), 0, sin(PI / 3.0))
+)
+
 
 func _ready():
+	print("RADIUS_VERTEX: ", RADIUS_VERTEX)
+	print("RADIUS_FACE: ", RADIUS_FACE)
+	print("RADIUS_EDGE: ", RADIUS_EDGE)
+	print("ANGLE: ", ANGLE)
+
 	for tri in tris:
 		var pos = Tri.tri_center(tri)
 		print(tri, " ", pos)
@@ -46,15 +61,31 @@ func do_update():
 	print("points up: ", Tri.points_up(tri_pos))
 	print("tri_pos: ", tri_pos)
 	print("cart_pos: ", pos)
+	ico.translation.x = $Indicator.translation.x
+	ico.translation.z = $Indicator.translation.z
+
 
 func move(delta: Vector3):
 	tri_pos += delta
+	var rot_axis = rotation_mat3.xform(delta)
+	ico.rotate(rot_axis, ANGLE)
 	do_update()
 
+
+func move2(delta: Vector3):
+#	move(delta)
+	ico.rotate(delta, ANGLE)
+
+
 func _input(event):
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
+
 	if event.is_action_pressed("action"):
 		index += 1
 		do_update()
+
+		ico.rotate(Vector3.LEFT, ANGLE)
 
 	if event.is_action_pressed("move_left"):
 		if Tri.points_up(tri_pos):
@@ -82,14 +113,18 @@ func _input(event):
 			return
 		match event.scancode:
 			KEY_R:
-				move(Vector3(-1, 0, 0))
+				move2(Vector3(cos(PI / 3.0), 0, sin(PI / 3.0)))
 			KEY_T:
-				move(Vector3(1, 0, 0))
+				move2(Vector3(-cos(PI / 3.0), 0, -sin(PI / 3.0)))
 			KEY_F:
-				move(Vector3(0, -1, 0))
+				move2(Vector3(cos(PI / 3.0), 0, -sin(PI / 3.0)))
 			KEY_G:
-				move(Vector3(0, 1, 0))
+				move2(Vector3(-cos(PI / 3.0), 0, sin(PI / 3.0)))
 			KEY_V:
-				move(Vector3(0, 0, -1))
+				move2(Vector3(-1, 0, 0))
 			KEY_B:
-				move(Vector3(0, 0, 1))
+				move2(Vector3(1, 0, 0))
+			KEY_U:
+				ico.transform.basis = Basis()
+				tri_pos = TRI_POS_START
+				do_update()
