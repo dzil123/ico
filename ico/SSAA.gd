3674ce296d59826311be76eef0d738b5d8e5e8c5
dzil123 <5725958+dzil123@users.noreply.github.com>
2022-02-26 23:37:22 -0800

    rotate portrait

--- ico/SSAA.gd
+++ ico/SSAA.gd
@@ -5,6 +5,8 @@ export(SSAA) var ssaa = SSAA.x4
 
 onready var viewport_outer = $ViewportOuter
 onready var viewport_inner = $ViewportOuter/ViewportInnerContainer/ViewportInner
+onready var root = $".."
+onready var is_portrait = tell_portrait()
 
 const SCALE_LOOKUP = [Vector2(1, 1), Vector2(1, 2), Vector2(2, 4)]
 
@@ -19,18 +21,38 @@ func _ready():
 
 
 func apply_ssaa():
-	print("scale: ", OS.get_screen_scale())
-	if OS.get_screen_scale() > 1.5:
+	var screen_scale = OS.get_screen_scale()
+	print("screen_scale: ", screen_scale)
+	if screen_scale > 1.5:
 		ssaa = SSAA.x1
 		print("no ssaa")
 	print("ssaa: ", ssaa)
 
-	_root_viewport_size_changed()
+	call_deferred("_root_viewport_size_changed")
 
 
 func _root_viewport_size_changed():
-	var size = get_viewport().size
+	var raw_size = get_viewport().size
+	var flipped_size = Vector2(raw_size.y, raw_size.x)
+	var size: Vector2  # landscape size of the 3d viewport
+
+	if is_portrait:
+		size = flipped_size
+		root.rect_position.x = size.y
+		root.rect_rotation = 90
+	else:
+		size = raw_size
+		root.rect_position.x = 0
+		root.rect_rotation = 0
+
+	root.rect_size = size
+
 	var scale = SCALE_LOOKUP[ssaa]
 
 	viewport_outer.size = size * scale.x
 	viewport_inner.size = size * scale.y
+
+
+func tell_portrait():
+	var size = get_viewport().size
+	return size.y > size.x
