18ed84e494fa986bed80a152180abad160b41cc1
dzil123 <5725958+dzil123@users.noreply.github.com>
2022-02-27 19:28:18 -0800

    first pass on touchscreen gestures

---
+++ ico/scripts/Gestures.gd
@@ -0,0 +1,44 @@
+extends Node
+
+var finger_map := []
+
+
+func _ready():
+	for _i in range(10):
+		finger_map.append(null)
+
+
+func dir_to_action(dir):
+	print(dir)
+	match dir:
+		Vector2(-1, 0):
+			return "move_left"
+		Vector2(1, 0):
+			return "move_right"
+		Vector2(0, -1):
+			return "move_up"
+		Vector2(0, 1):
+			return "move_down"
+		_:
+			return null
+
+
+func _input(event):
+	if event is InputEventScreenDrag:
+		if finger_map[event.index] == null:
+			return
+		var s = event.speed.snapped(Vector2.ONE * 1000)
+		if s != Vector2.ZERO:
+			s = s.normalized().snapped(Vector2.ONE)
+			var action = dir_to_action(s)
+			print(action)
+			if action != null:
+				print(action, " ", s)
+				Input.action_press(action)
+				finger_map[event.index] = null
+
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			finger_map[event.index] = event.position
+		else:
+			finger_map[event.index] = null
